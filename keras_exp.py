99c38a3c9fb5cb5b32099a18c221862d1f30aae3
Jordan Reed <reed5204@vandals.uidaho.edu>
2024-11-29 17:13:09 -0800

    added code to build nn and cnn

--- keras_exp.py
+++ keras_exp.py
@@ -42,22 +42,6 @@ def build_pointnet_model(filepath=None):
     model.summary()
     return model
 
-def build_pointconv_model(batch_size, input_shape):
-    model = PointConvModel(batch_size, num_classes=2)
-    model.build(batch_size, input_shape)
-
-    return model
-
-def save_data(filename, model_name, scores, parameters):
-    with open(filename, 'w') as f:
-        f.write(f'Model,{model_name},\n')
-        f.write(f'Parameters,\n')
-        for i, param in enumerate(parameters):
-            f.write(f'{param},')
-            if (i-1)%2 == 0:
-                f.write('\n')
-        f.write(f'\nTrain Score,{scores[0]},Test score,{scores[1]},\n')
-
 def build_dgcnn_model(input_shape=None, filepath=None):
     if filepath is not None and os.path.exists(filepath):
         print(f'Loading dgcnn model from {filepath}')
@@ -82,6 +66,103 @@ def build_dgcnn_model(input_shape=None, filepath=None):
         model = keras.Model(inputs=[X, E], outputs=output)
 
     return model
+
+def data_prep_cnn(train_imgs, test_imgs):
+    """
+    Change shape of images for convolutional network.
+
+    :param train_imgs: np array of training images
+    :param test_imgs: np array of test images
+    :return: fixed arrays of training images and test images
+    """
+    cnn_train_imgs = np.expand_dims(train_imgs, -1)
+    cnn_test_imgs = np.expand_dims(test_imgs, -1)
+
+    return cnn_train_imgs, cnn_test_imgs
+
+def build_cnn_model(num_conv_layers, input_size, output_size):
+    """
+    Assemble appropriate layers for a convolutional neural network.
+    - Input layer
+    - n amount of 'hidden' layers 
+      - consists of a convolutional layer and a max pooling layer
+    - Flatten, dropout, and softmax layer
+
+    :param num_conv_layers: number of convolutional layers
+    :param input_size: tuple of input shape
+    :param output_size: number of categories
+    :return: built model
+    """
+    layers = []
+
+    # input layer
+    layers.append(keras.Input(shape=input_size))
+
+    # convolutional layer
+    for i in range(0, num_conv_layers):
+        layers.append(keras.layers.Conv2D(32, kernel_size=(3,3), activation='relu'))
+        layers.append(keras.layers.MaxPool2D(pool_size=(2,2)))
+    
+    # transform to NN
+    layers.append(keras.layers.Flatten())
+    layers.append(keras.layers.Dropout(.5))
+    layers.append(keras.layers.Dense(output_size, activation='softmax'))
+
+    model = keras.models.Sequential(layers)
+    model.summary()
+
+    return model
+
+def build_fcnn_model(num_layers:int, num_nodes:list, activation:str, input_size, output_size):
+    """
+    Build a fully connected neural network with the parameters given
+
+    :param num_layers: number of hidden layers that should be created
+    :param num_nodes: number of nodes each hidden layer should have
+    :param activation: the activation function for each hidden layer
+    :param input_size: what size the input layer should have
+    :param output_size: what size the output layer should have
+    :return: a model
+    """
+    # num nodes is a list of ints
+    # list size must match num layers
+    if len(num_nodes) != num_layers:
+        print(f'node number list does not match: {len(num_nodes)} != {num_layers}')
+        exit(1)
+    
+    # build model
+    layers = []
+
+    # input layer - providing shape of data (not batches)
+    layers.append(keras.layers.Flatten(input_shape=input_size))
+
+    # hidden layers
+    for i in range(0, num_layers):
+        layers.append(keras.layers.Dense(num_nodes[i], activation=activation))
+
+    # output layer - same number of units as out output classes
+    layers.append(keras.layers.Dense(output_size, activation="softmax"))
+
+    # make model
+    model = keras.models.Sequential(layers)
+
+    # print model summary
+    print(model.summary())
+    
+    return model
+
+
+
+
+def save_data(filename, model_name, scores, parameters):
+    with open(filename, 'w') as f:
+        f.write(f'Model,{model_name},\n')
+        f.write(f'Parameters,\n')
+        for i, param in enumerate(parameters):
+            f.write(f'{param},')
+            if (i-1)%2 == 0:
+                f.write('\n')
+        f.write(f'\nTrain Score,{scores[0]},Test score,{scores[1]},\n')
 
 # ------ run experiments -------
 train_data, train_label, train_mask, test_data, test_label, test_mask = load_all_data()
