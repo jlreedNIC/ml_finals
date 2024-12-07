6f88c342e7b33a2a7c067758bcf8ddf39e60d9b5
Jordan Reed <reed5204@vandals.uidaho.edu>
2024-12-07 15:39:55 -0800

    changed code to handle addition of voxels. changed definition of sobel filters

--- mv_algos.py
+++ mv_algos.py
@@ -7,14 +7,16 @@
 #           put point clouds into grid and then do algos on grid
 # ------------------------
 import cv2 as cv
-from data_manip import load_data, test_file, show_point_clouds
+from data_manip import load_data, test_file, load_points_from_stl
 import numpy as np
 import open3d as o3d
 from voxel_custom import Custom_Voxel
 
-test_data, test_labels, test_mask = load_data(test_file)
+# test_data, test_labels, test_mask = load_data(test_file)
+# point_cloud = test_data[0]
 
-point_cloud = test_data[0]
+point_cloud = load_points_from_stl("data/tape.stl")
+
 
 def get_bounds_along_axis(point_cloud, coord):
     maximum = np.max(point_cloud[:,coord], axis=0)
@@ -103,6 +105,7 @@ def show_point_cloud_from_grids(grids:list, voxel_size):
     )
 
 def apply_3d_convolution_filter(occupancy_grid, filter_3d):
+    print(f'Applying convolution filter...')
     # new_array = np.zeros(occupancy_grid.shape)
     num_z_voxels = occupancy_grid.shape[2]
     num_y_voxels = occupancy_grid.shape[1]
@@ -146,51 +149,84 @@ def apply_3d_convolution_filter(occupancy_grid, filter_3d):
 
 # ------ main ----------
 # voxel_size = .015
-voxel_size = .05
+voxel_size = .01
+print('creating occupancy grid')
 occupancy_grid = create_occupancy_grid_of_voxels(point_cloud, voxel_size)
-print(occupancy_grid.shape)
+print(f'number of voxels: {occupancy_grid.shape}')
 
 # define sobel filters for x, y, z directions
 sobel_3dz_filter = [
-    [[0, -1,  0], 
-     [0, -2,  0], 
-     [0, -1,  0]],
-    [[1,  0, -1], 
-     [2,  0, -2], 
-     [1,  0, -1]],
-    [[0,  1,  0], 
-     [0,  2,  0], 
-     [0,  1,  0]],
+    [[-1, -1, -1], 
+     [-1, -2, -1], 
+     [-1, -1, -1]],
+    [[ 0,  0,  0], 
+     [ 0,  0,  0], 
+     [ 0,  0,  0]],
+    [[ 1,  1,  1], 
+     [ 1,  2,  1], 
+     [ 1,  1,  1]],
 ]
 sobel_3dy_filter = [
-    [[ 0,  0,  0], 
-     [-1, -2, -1], 
-     [ 0,  0,  0]],
+    [[-1, -1, -1], 
+     [ 0,  0,  0], 
+     [ 1,  1,  1]],
     [[-1, -2, -1], 
      [ 0,  0,  0], 
      [ 1,  2,  1]],
-    [[ 0,  0,  0], 
-     [ 1,  2,  1], 
-     [ 0,  0,  0]],
+    [[-1, -1, -1], 
+     [ 0,  0,  0], 
+     [ 1,  1,  1]],
 ]
 sobel_3dx_filter = [
-    [[ 0, -1,  0], 
-     [-1,  0,  1], 
-     [ 0,  1,  0]],
-    [[ 0, -2,  0], 
-     [-2,  0,  2], 
-     [ 0,  2,  0]],
-    [[ 0, -1,  0], 
-     [-1,  0,  1], 
-     [ 0,  1,  0]],
+    [[ 1,  0, -1], 
+     [ 1,  0, -1], 
+     [ 1,  0, -1]],
+    [[ 1,  0, -1], 
+     [ 2,  0, -2], 
+     [ 1,  0, -1]],
+    [[ 1,  0, -1], 
+     [ 1,  0, -1], 
+     [ 1,  0, -1]],
 ]
 
 # 3d convolution
 
 sobel_z = apply_3d_convolution_filter(occupancy_grid, sobel_3dz_filter)
+sobel_y = apply_3d_convolution_filter(occupancy_grid, sobel_3dy_filter)
+sobel_x = apply_3d_convolution_filter(occupancy_grid, sobel_3dx_filter)
 # show_point_cloud_from_grid([occupancy_grid], voxel_size)
 # show_point_cloud_from_grid([sobel_z], voxel_size)
-show_point_cloud_from_grids([occupancy_grid, sobel_z], voxel_size) # this isn't showing 2 grids at once
+# show_point_cloud_from_grids([occupancy_grid, sobel_z], voxel_size) # this isn't showing 2 grids at once
+# show_point_cloud_from_grids([occupancy_grid, sobel_y], voxel_size) # this isn't showing 2 grids at once
+# show_point_cloud_from_grids([occupancy_grid, sobel_x], voxel_size) # this isn't showing 2 grids at once
+
+# num_z_voxels = occupancy_grid.shape[2]
+# num_y_voxels = occupancy_grid.shape[1]
+# num_x_voxels = occupancy_grid.shape[0]
+# combined_sobels = [[[Custom_Voxel(voxel_size) for i in range(num_z_voxels)] for j in range(num_y_voxels)] for k in range(num_x_voxels)]
+# # combine sobel grids
+# for i in range(len(sobel_x)):
+#     for j in range(len(sobel_x[i])):
+#         for k in range(len(sobel_x[i][j])):
+#             combined_sobels[i][j][k].add_points(sobel_x[i][j][k].point_list)
+#             combined_sobels[i][j][k].add_points(sobel_y[i][j][k].point_list)
+#             combined_sobels[i][j][k].add_points(sobel_z[i][j][k].point_list)
+
+#             # combined_sobels[i][j][k].value = 0
+#             combined_sobels[i][j][k].value = sobel_x[i][j][k].value
+#             combined_sobels[i][j][k].value += sobel_y[i][j][k].value
+#             combined_sobels[i][j][k].value += sobel_z[i][j][k].value
+
+combined_sobels = sobel_x + sobel_y + sobel_z
+combined_sobels = np.add(sobel_x, sobel_y)
+combined_sobels = np.add(combined_sobels, sobel_z)
+# threshold = np.max(combined_sobels)*.75
+# combined_sobels = combined_sobels[combined_sobels>=threshold]
+# combined_sobels = np.array(combined_sobels)
+print(combined_sobels.shape)
+# print(combined_sobels[0][0][0])
+
+show_point_cloud_from_grids([occupancy_grid, combined_sobels], voxel_size)
 
 
 
